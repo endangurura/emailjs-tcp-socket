c7835a1bf1abf7a39314d5f556989a88a199a5a5
Emmanuel Ndangurura <endangurura@tachyinc.com>
2021-08-28 16:00:18 +0300

    remove node-socket

--- src/socket.js
+++ src/socket.js
@@ -6,9 +6,21 @@ class DummySocket {
   }
 }
 
+/*
 if (typeof process !== 'undefined') {
   TCPSocket = require('./node-socket')
 } else if (typeof chrome !== 'undefined' && (chrome.socket || chrome.sockets)) {
+  TCPSocket = require('./chrome-socket')
+} else if (typeof Windows === 'object' && Windows && Windows.Networking && Windows.Networking.Sockets && Windows.Networking.Sockets.StreamSocket) {
+  TCPSocket = require('./windows-socket')
+} else if (typeof window === 'object' && typeof io === 'function') {
+  TCPSocket = require('./socketio-socket')
+} else {
+  TCPSocket = DummySocket
+}
+*/
+
+if (typeof chrome !== 'undefined' && (chrome.socket || chrome.sockets)) {
   TCPSocket = require('./chrome-socket')
 } else if (typeof Windows === 'object' && Windows && Windows.Networking && Windows.Networking.Sockets && Windows.Networking.Sockets.StreamSocket) {
   TCPSocket = require('./windows-socket')
